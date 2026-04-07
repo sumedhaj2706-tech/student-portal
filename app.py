84a745047de5e4a7f76c62165e10da834860cff4
sumedha <sumedhaj2706@gmail.com>
2026-04-07 17:36:29 +0530

    Initial commit - student portal

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Temporary storage (list)
+submissions = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    usn = request.form['usn']
+    title = request.form['title']
+    link = request.form['link']
+
+    # Store data
+    submissions.append({
+        "name": name,
+        "usn": usn,
+        "title": title,
+        "link": link
+    })
+
+    return f"""
+    <h3>Submitted Successfully!</h3>
+    <a href="/">Go Back</a>
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True)
